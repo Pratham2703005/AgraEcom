5e6adc082939a7bff92a46382ee3417af8a55e24
Pratham Israni <pk2732004@gmail.com>
2025-07-22 16:11:44 +0530

    type fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -23,7 +23,7 @@ export default function NotFound() {
         </h1>
         
         <p className="text-lg text-neutral-600 dark:text-neutral-400 mb-8 max-w-md mx-auto">
-          Sorry, we couldn't find the page you're looking for. It might have been moved, deleted, or you entered the wrong URL.
+          Sorry, we couldn&apos;t find the page you&apos;re looking for. It might have been moved, deleted, or you entered the wrong URL.
         </p>
 
         {/* Action Buttons */}
